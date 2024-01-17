0939842313f0966a9d6aa016f0b5b05c99876097
Mffedai <mfatihfedai@gmail.com>
2024-01-17 20:11:39 +0300

    employee comment

--- Employees/src/Main.java
+++ Employees/src/Main.java
@@ -1,19 +1,21 @@
 import java.sql.*;
 
 public class Main {
+    //Veritabanı bilgileri alınır.
     public static final String DB_URL = "jdbc:mysql://localhost/company";
     public static final String DB_USER = "root";
     public static final String DB_PASSWORD = "mysql";
 
     public static void main(String[] args) {
 
-
         Connection connection = null;
         Statement statement = null;
         try {
+            //Veritabanı bilgileri ile java arasındaki bağlantı kurulur.
             connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM employees");
+            //Veritabanındaki bilgilerin ekrana basılması sağlanır.
             while(resultSet.next()){
                 System.out.println("ID : " + resultSet.getInt("employees_id"));
                 System.out.println("Name : " + resultSet.getString("employees_name"));
@@ -22,6 +24,7 @@ public class Main {
                 System.out.println("###############");
             }
         } catch (SQLException e) {
+            //Eğer hata oluşursa hata açıklanır.
             System.out.println("SQLException: " + e.getMessage());
             System.out.println("SQLState: " + e.getSQLState());
             System.out.println("VendorError: " + e.getErrorCode());
